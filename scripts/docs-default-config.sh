139413c09b08d08080da780d6d8bcd9d61c3dbdd
jprivet-dev <jprivet.dev@gmail.com>
2025-09-18 22:13:27 +0200

    docs(monolog): update default config parts

--- scripts/docs-default-config.sh
+++ scripts/docs-default-config.sh
@@ -10,11 +10,11 @@ R='\033[31m'
 Y='\033[33m'
 S='\033[0m'
 
-make monolog_default >docs/monolog/yaml/default-config-new-syntax.yaml
-sed -i '1d' docs/monolog/yaml/default-config-new-syntax.yaml # Remove firs line of the file
+make monolog_default >docs/monolog/yaml/monolog-default-config-new-syntax.yaml
+sed -i '1d' docs/monolog/yaml/monolog-default-config-new-syntax.yaml # Remove firs line of the file
 printf " ${G}✔${S} default-config-new-syntax.yaml has been updated successfully.\n"
 
-make monolog_default_xml >docs/monolog/xml/default-config-new-syntax.xml
-sed -i '1d' docs/monolog/xml/default-config-new-syntax.xml # Remove firs line of the file
+make monolog_default_xml >docs/monolog/xml/monolog-default-config-new-syntax.xml
+sed -i '1d' docs/monolog/xml/monolog-default-config-new-syntax.xml # Remove firs line of the file
 printf " ${G}✔${S} default-config-new-syntax.xml has been updated successfully.\n"
 
