2dde2bbf774aa2209d7866ffb5b628309f31f36a
jprivet-dev <jprivet.dev@gmail.com>
2025-09-18 21:59:34 +0200

    docs(monolog): generate default config (yaml & xml formatsÃ)

--- scripts/docs-default-config.sh
+++ scripts/docs-default-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Usage:
-#   . docs-default-config.sh
+#   . script/docs-default-config.sh
 # or
-#   source docs-default-config.sh
+#   source script/docs-default-config.sh
 
 # (G)REEN, (R)ED, (Y)ELLOW & RE(S)ET
 G='\033[32m'
@@ -10,10 +10,11 @@ R='\033[31m'
 Y='\033[33m'
 S='\033[0m'
 
-# Export MonologBundle default configuration
-make monolog_default_config >docs/monolog/yaml/default-config-new-syntax.yaml
-
-# Remove firs line of the file
-sed -i '1d' docs/monolog/yaml/default-config-new-syntax.yaml
-
+make monolog_default >docs/monolog/yaml/default-config-new-syntax.yaml
+sed -i '1d' docs/monolog/yaml/default-config-new-syntax.yaml # Remove firs line of the file
 printf " ${G}✔${S} default-config-new-syntax.yaml has been updated successfully.\n"
+
+make monolog_default_xml >docs/monolog/xml/default-config-new-syntax.xml
+sed -i '1d' docs/monolog/xml/default-config-new-syntax.xml # Remove firs line of the file
+printf " ${G}✔${S} default-config-new-syntax.xml has been updated successfully.\n"
+
